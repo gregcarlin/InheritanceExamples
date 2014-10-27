661099b49a5eff29483932817607ac9bb4a39aa2
Brian <brian@convolve.com>
2014-10-27 16:52:49 -0400

    a

--- src/org/byramhills/apcs/cars/AstonMartin.java
+++ src/org/byramhills/apcs/cars/AstonMartin.java
@@ -10,11 +10,11 @@ public class AstonMartin extends Car {
 		super();
 	}
 
-	public AstonMartin(boolean jamesBond, int gadgetCount1, int currentSpeed1,
+	public AstonMartin(boolean jamesBond1, int gadgetCount1, int currentSpeed1,
 			int maxSpeed1, String color1, boolean cool1) {
 		super(currentSpeed1, maxSpeed1, color1, cool1);
-		loud = loud1;
-		howFun = howFun2;
+		jamesBond = jamesBond1;
+		gadgetCount = gadgetCount1;
 	}
 
 	// Override the old drive to make it fast
--- src/org/byramhills/apcs/cars/tester.java
+++ src/org/byramhills/apcs/cars/tester.java
@@ -9,6 +9,9 @@ public class tester {
 		Ferrari brian = new Ferrari();
 		Ferrari brian2 = new Ferrari(true, "very", 0, 250, "red", true);
 		brian2.drive();
+
+		AstonMartin lewick = new AstonMartin();
+		AstonMartin lewick2 = new AstonMartin(true, 7, 0, 250, "green", true);
 	}
 
 }
