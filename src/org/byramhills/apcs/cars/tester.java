af7d7d564bcbbc7d9697442ce4de37879983a719
Brian <brian@convolve.com>
2014-10-27 08:23:23 -0400

    more stuff

---
+++ src/org/byramhills/apcs/cars/tester.java
@@ -0,0 +1,14 @@
+package org.byramhills.apcs.cars;
+
+public class tester {
+
+	public static void main(String[] args) {
+		// Cant initiate abstract classes
+		Car carlin = new Car();
+
+		Ferrari brian = new Ferrari();
+		Ferrari brian2 = new Ferrari(true, "very", 0, 250, "red", true);
+		brian2.drive();
+	}
+
+}
